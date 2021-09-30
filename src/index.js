1fd8b0726b9b1a8601b0ce26e2b30d26323611c1
Linacolor <annafragile123@gmail.com>
2021-09-30 23:29:20 +0300

    Добавляет изменение темы

--- src/index.js
+++ src/index.js
@@ -10,3 +10,37 @@ function createMenuMarkup(menu) {
 }
 const menuMarkup = createMenuMarkup(menu);
 menuEl.insertAdjacentHTML('beforeend', menuMarkup);
+
+//THEME//
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+function changeThemePage() {
+  let themePage = Theme[localStorage.getItem('theme')];
+  if (themePage) {
+    bodyEl.classList.add(themePage);
+    if (localStorage.getItem('theme') == 'LIGHT') {
+      checkboxEl.checked = false;
+    } else {
+      checkboxEl.checked = true;
+    }
+  } else {
+    bodyEl.classList.add(Theme[0]);
+    localStorage.setItem('theme', Theme[0]);
+  }
+}
+
+function setTheme() {
+  if (bodyEl.classList.contains('light-theme')) {
+    bodyEl.classList.replace('light-theme', 'dark-theme');
+    localStorage.setItem('theme', 'DARK');
+  } else {
+    bodyEl.classList.replace('dark-theme', 'light-theme');
+    localStorage.setItem('theme', 'LIGHT');
+  }
+}
+
+changeThemePage();
+checkboxEl.addEventListener('change', setTheme);
